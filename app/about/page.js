3bf79cdb164e802187063506fe7d3a4d362afe76
ShashikalaDil <shashi05dilru@gmail.com>
2024-10-04 15:12:03 +0530

    Initial commit

---
+++ app/about/page.js
@@ -0,0 +1,13 @@
+import Nav from "@/components/Nav";
+
+export default function About() {
+  return (
+    <>
+    <div className="flex p-24">
+        <Nav/>
+        <h1>About</h1>
+    </div>
+     
+    </>
+  );
+}
